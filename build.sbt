fc60b9dc00b47d936d265386f8e6be18b2209fd6
Yaroslav Klymko <t3hnar@gmail.com>
2017-03-20 08:00:18 +0200

    PL-5056 add MetricHelper

---
+++ build.sbt
@@ -0,0 +1,36 @@
+name := "metric-tools"
+
+organization := "com.evolutiongaming"
+
+homepage := Some(new URL("http://github.com/evolution-gaming/metric-tools"))
+
+startYear := Some(2017)
+
+organizationName := "Evolution Gaming"
+
+organizationHomepage := Some(url("http://evolutiongaming.com"))
+
+bintrayOrganization := Some("evolutiongaming")
+
+scalaVersion := "2.11.8"
+
+scalacOptions ++= Seq(
+  "-encoding", "UTF-8",
+  "-feature",
+  "-unchecked",
+  "-deprecation",
+  "-Xfatal-warnings",
+  "-Xlint",
+  "-Yno-adapted-args",
+  "-Ywarn-dead-code",
+  "-Ywarn-numeric-widen",
+  "-Xfuture"
+)
+
+scalacOptions in (Compile,doc) ++= Seq("-groups", "-implicits", "-no-link-warnings")
+
+libraryDependencies ++= Seq(
+  "io.dropwizard.metrics" % "metrics-core" % "3.1.2"
+)
+
+licenses := Seq(("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0")))
---
+++ src/main/scala/com/evolutiongaming/util/MetricHelper.scala
@@ -0,0 +1,56 @@
+package com.evolutiongaming.util
+
+import com.codahale.metrics.{Gauge, Histogram, MetricRegistry, Timer}
+
+import scala.compat.Platform
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.control.NonFatal
+
+object MetricHelper {
+
+  object GaugeF {
+    def apply[T](f: => T): Gauge[T] = new Gauge[T] { def getValue: T = f }
+  }
+
+  implicit class RichTimer(val timer: Timer) extends AnyVal {
+    def timeExceed[T](limit: FiniteDuration)(f: => T): T = {
+      val start = Platform.currentTime
+      try f finally {
+        val stop = Platform.currentTime
+        val duration = stop - start
+        if (duration >= limit.toMillis) timer.update(duration, MILLISECONDS)
+      }
+    }
+
+    def timeFuture[T](f: => Future[T])(implicit ec: ExecutionContext): Future[T] = {
+      val time = timer.time()
+      try f andThen { case _ => time.stop() } catch {
+        case NonFatal(e) => time.stop(); throw e
+      }
+    }
+
+    def timeFunc[T](f: => T): T = {
+      val time = timer.time()
+      try f finally time.stop()
+    }
+  }
+
+  implicit class HistogramOps(val histogram: Histogram) extends AnyVal {
+    def timeFunc[T](f: => T): T = {
+      val start = Platform.currentTime
+      try f finally histogram.update(Platform.currentTime - start)
+    }
+
+    def timeFuture[T](f: => Future[T])(implicit ec: ExecutionContext): Future[T] = {
+      val start = Platform.currentTime
+      f andThen { case _ => histogram.update(Platform.currentTime - start) }
+    }
+  }
+
+  implicit class MetricRegistryOps(val self: MetricRegistry) extends AnyVal {
+    def gauge[T](name: String, f: => T): Gauge[T] = {
+      self.register(name, GaugeF(f))
+    }
+  }
+}
